830ae99db01735e9843c8bfcd0ab4110fd0ec879
danpinto97 <dpinto@stevens.edu>
2021-06-26 20:03:58 -0400

    Get rid of ugly wtforms in favor of Chad bootstrap forms.

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, request
 from flask_wtf import form
 from forms import LoginForm, TextForm, NumForm
 from config import Config
 from dbsubmission import BasicSubmission
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    form = TextForm()
-    if form.validate_on_submit():
-        basicSubmission = BasicSubmission(form.answer1.data)
-        print(form.answer1.data)
-        return redirect('/home')
-    return render_template('index.html', title='Form', form=form)
+    return render_template('index.html', title='Form')
 
-@app.route('/home')
+@app.route('/home', methods= ['POST'])
 def home():
+    q1 = request.form.get("question1")
+    ch1 = request.form.get("check1")
+    ch2 = request.form.get("check2")
+    print(q1)
+    print(ch1)
+    print(ch2)
     return render_template('home.html')
 
 if __name__ == "__main__":
